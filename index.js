0bc01886d20535705b37452737130421a4eb2d68
declantyson <declantyson@hotmail.co.uk>
2017-09-05 17:44:58 +0100

    Only download landscape images none of that portrait bullshit

--- index.js
+++ index.js
@@ -7,16 +7,30 @@ const backgroundEndpoint = "https://www.reddit.com/r/EarthPorn+SpacePorn+RoomPor
 request.get(backgroundEndpoint, function (err, res, body) {
     let i = Math.floor(Math.random() * 10),
         json = JSON.parse(body),
-        image = json.data.children[i].data.url;
+        imageData = json.data.children[i].data,
+        image = imageData.url;
 
-    fs.readFile('assets/bg-count.js', 'utf-8', function (err, content) {
-        let bgCount = parseInt(content.replace("var bgCount = ", "").replace(";", ""));
+    if(imageData.preview.images.length === 0) {
+        console.log("Cannot determine if image is appropraite, skipping. Please run again.");
+    } else {
+        let resolutions = imageData.preview.images[0].resolutions;
+        if(resolutions.length < 0) {
+            console.log("Cannot determine if image is appropraite, skipping. Please run again.");
+        } else if(resolutions[0].height > resolutions[0].width) {
+            console.log("Image is portrait, skipping. Please run again.");
+        } else {
+            fs.readFile('assets/bg-count.js', 'utf-8', function (err, content) {
+                let bgCount = parseInt(content.replace("var bgCount = ", "").replace(";", ""));
 
-        fs.writeFile('assets/bg-count.js', "var bgCount = " + (bgCount + 1) + ";", function (err) {
-            let path = "assets/backgrounds/" + bgCount + ".png";
-            request(image).pipe(fs.createWriteStream(path)).on('close', function (err) {
-                console.log("Downloaded " + image);
+                let path = "assets/backgrounds/" + bgCount + ".png";
+                request(image).pipe(fs.createWriteStream(path)).on('close', function (err) {
+
+                    bgCount++;
+                    fs.writeFile('assets/bg-count.js', "var bgCount = " + (bgCount) + ";", function (err) {
+                        console.log("Downloaded " + image);
+                    });
+                });
             });
-        });
-    });
+        }
+    }
 });
