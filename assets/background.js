f745489eafd1846747939f8f2e38f53ed1ba5d0b
declantyson <declantyson@hotmail.co.uk>
2017-09-15 16:36:19 +0100

    Add cinemagraph backgrounds

--- assets/background.js
+++ assets/background.js
@@ -1,10 +1,25 @@
 function get_background() {
-    var i = Math.floor(Math.random() * bgCount);
-    image = "assets/backgrounds/" + i + ".png",
+    var i = Math.floor(Math.random() * bgCount),
+        image = "assets/backgrounds/" + i + ".png",
         loader = document.createElement('img');
 
     loader.setAttribute('src', image);
     loader.onload = function() {
         document.getElementsByTagName('body')[0].style.backgroundImage = "url(" + image + ")";
+    };
+}
+
+function get_cinematic_background() {
+    var i = Math.floor(Math.random() * cinematicBgCount),
+        source = "assets/backgrounds-cinemagraphs/" + i + ".mp4",
+        video = document.createElement('video');
+
+    video.setAttribute('src', source);
+    video.onloadeddata = function () {
+        var bgVideo = document.getElementById('cinematic-video');
+        bgVideo.setAttribute('src', source);
+        bgVideo.className = "visible";
+
+        console.log("!");
     };
 }
---
+++ assets/cinematic-bg-count.js
@@ -0,0 +1,1 @@
+var bgCount = 8;
