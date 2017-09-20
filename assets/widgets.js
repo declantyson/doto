a6644b935dc0cb8b1fcddf2cb82a34f994b1ad15
declantyson <declantyson@hotmail.co.uk>
2017-09-20 16:03:14 +0100

    News ticker

--- assets/widgets.js
+++ assets/widgets.js
@@ -39,3 +39,35 @@ function weather() {
         }
     );
 }
+
+function news() {
+    var sources = [
+            "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=e0fddafbeb304299913237a89228f313",
+            "https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=e0fddafbeb304299913237a89228f313",
+            "https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=e0fddafbeb304299913237a89228f313"
+        ],
+        source = sources[Math.floor(Math.random() * sources.length)];
+
+
+    widget(source,
+        function (data) {
+            var news = document.getElementById('news'),
+                title = document.createElement('p'),
+                description = document.createElement('small'),
+                article = data.articles[Math.floor(Math.random() * data.articles.length)];
+
+            news.className = "";
+            setTimeout(function () {
+                news.innerHTML = "";
+                news.className = "visible";
+                title.innerText = article.title;
+                description.innerText = article.description;
+
+                news.appendChild(title).appendChild(description);
+            }, 500);
+        },
+        function () {
+            
+        }
+    )
+}
--- start.js
+++ start.js
@@ -1,8 +1,10 @@
 get_cinematic_background();
 initialize_clock();
 weather();
+news();
 
 setInterval(weather, 5000);
+setInterval(news, 5000);
 
 if(typeof lightdm !== "undefined") {
     build_display();
